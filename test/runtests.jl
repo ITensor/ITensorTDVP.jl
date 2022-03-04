66006e60499a73dab9b2540b0ad42b6c94eb1862
Miles <miles.stoudenmire@gmail.com>
2022-03-03 20:37:36 -0500

    Added a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,43 @@
+using ITensors
 using ITensorTDVP
 using Test
 
 @testset "ITensorTDVP.jl" begin
-    # Write your tests here.
+
+@testset "Basic TDVP" begin
+  N = 10
+  exponentiate_krylovdim = 20
+  s = siteinds("S=1/2", N)
+
+  os = OpSum()
+  for j in 1:(N - 1)
+    os += 0.5, "S+", j, "S-", j + 1
+    os += 0.5, "S-", j, "S+", j + 1
+    os += "Sz", j, "Sz", j + 1
+  end
+
+  H = MPO(os, s)
+
+  ψ0 = randomMPS(s, "↑"; linkdims=10)
+
+  ψ1 = tdvp(H, ψ0, -0.1im; exponentiate_krylovdim=exponentiate_krylovdim,
+                             nsweeps=1, maxdim=50, cutoff=1e-10)
+
+  # Should lose fidelity:
+  @test abs(inner(ψ0,ψ1)) < 0.5
+
+  # Average energy should be conserved:
+  @test real(inner(ψ1,H,ψ1)) ≈ inner(ψ0,H,ψ0)
+
+  # Time evolve backwards:
+  ψ2 = tdvp(H, ψ1, +0.1im; nsweeps=1, maxdim=50, cutoff=1e-10)
+
+  # Should rotate back to original state:
+  @test abs(inner(ψ0,ψ2)) ≈ 1.0
+
 end
+
+
+end
+
+nothing
