2be1ce9342463cca548cf224983c00996a2fd552
Matt Fishman <mtfishman@users.noreply.github.com>
2022-04-07 11:37:29 -0400

    Update test/tdvp.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/tdvp.jl
+++ test/tdvp.jl
@@ -299,7 +299,7 @@ end
   trange = 0.0:tau:ttotal
   for (step, t) in enumerate(trange)
     nsite = (step <= 10 ? 2 : 1)
-    psi = tdvp(H, -tau,psi; cutoff, nsite, normalize=true, exponentiate_krylovdim=15)
+    psi = tdvp(H, -tau, psi; cutoff, nsite, normalize=true, exponentiate_krylovdim=15)
     @printf("%.3f energy = %.12f\n", step * tau, inner(psi', H, psi))
   end
   #@show maxlinkdim(psi)
