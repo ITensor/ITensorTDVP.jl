509d054da921103c38b3b0b1a88016b3683192da
Matt Fishman <mtfishman@users.noreply.github.com>
2022-04-11 14:24:21 -0400

    Update examples/03_utils.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- examples/03_utils.jl
+++ examples/03_utils.jl
@@ -91,8 +91,8 @@ function heisenberg(n; J=1.0, J2=0.0)
   ℋ = OpSum()
   if !iszero(J)
     for j in 1:(n - 1)
-      ℋ += J/2, "S+", j, "S-", j + 1
-      ℋ += J/2, "S-", j, "S+", j + 1
+      ℋ += J / 2, "S+", j, "S-", j + 1
+      ℋ += J / 2, "S-", j, "S+", j + 1
       ℋ += J, "Sz", j, "Sz", j + 1
     end
   end
