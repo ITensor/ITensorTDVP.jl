cdacf63bf62ef56e9424358cc4deab38c08b03e8
Miles <miles.stoudenmire@gmail.com>
2022-10-04 17:04:45 -0400

    Remove tests for cases we may not want

--- test/test_tdvp.jl
+++ test/test_tdvp.jl
@@ -116,9 +116,6 @@ end
 
   ψ1 = tdvp(solver, H, -0.1im, ψ0; cutoff, nsite=1)
 
-  @test ψ1 ≈ tdvp(solver, -0.1im, H, ψ0; cutoff, nsite=1)
-  @test ψ1 ≈ tdvp(solver, H, ψ0, -0.1im; cutoff, nsite=1)
-
   @test norm(ψ1) ≈ 1.0
 
   ## Should lose fidelity:
