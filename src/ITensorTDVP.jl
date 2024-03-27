5ea19fce5e2ba88e187358cedb11fcf685a31ac3
Miles <miles.stoudenmire@gmail.com>
2024-03-27 11:38:30 +0900

    Remove code, which is moved to ITensors.ITensorMPS

--- src/ITensorTDVP.jl
+++ src/ITensorTDVP.jl
@@ -1,43 +1,22 @@
 module ITensorTDVP
 
 using ITensors
-using KrylovKit: exponentiate, eigsolve
-using KrylovKit: KrylovKit
-import KrylovKit: linsolve
+using KrylovKit: exponentiate, eigsolve, KrylovKit
 using Printf
 using TimerOutputs
 using Observers
 
-using ITensors:
-  AbstractMPS,
-  @debug_check,
-  @timeit_debug,
-  check_hascommoninds,
-  orthocenter,
-  ProjMPS,
-  set_nsite!
+#using ITensors:
+#  AbstractMPS,
+#  @debug_check,
+#  @timeit_debug,
+#  check_hascommoninds,
+#  orthocenter,
+#  ProjMPS,
+#  set_nsite!
 
-# Compatibility of ITensor observer and Observers
-include("update_observer.jl")
-
-# Utilities for making it easier
-# to define solvers (like ODE solvers)
-# for TDVP
-include("solver_utils.jl")
-
-include("applyexp.jl")
-include("tdvporder.jl")
-include("tdvpinfo.jl")
-include("tdvp_step.jl")
-include("tdvp_generic.jl")
-include("tdvp.jl")
-include("dmrg.jl")
-include("dmrg_x.jl")
-include("projmpo_apply.jl")
-include("contract_mpo_mps.jl")
-include("projmps2.jl")
-include("projmpo_mps2.jl")
-include("linsolve.jl")
+# Implementations have been moved to ITensors.ITensorMPS
+include("wrap_ITensorMPS.jl")
 
 export tdvp, dmrg_x, to_vec, TimeDependentSum, linsolve
 
