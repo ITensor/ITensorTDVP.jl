40ce92c2ae695ad0aea82a4a47358a8c2e95116d
Miles <miles.stoudenmire@gmail.com>
2024-03-27 15:34:23 +0900

    Remove codes, now moved to ITensors.ITensorMPS

--- src/ITensorTDVP.jl
+++ src/ITensorTDVP.jl
@@ -1,19 +1,9 @@
 module ITensorTDVP
-export TimeDependentSum, dmrg_x, linsolve, tdvp, to_vec
 
-include("defaults.jl")
-include("update_observer.jl")
-include("solver_utils.jl")
-include("tdvporder.jl")
-include("tdvpinfo.jl")
-include("sweep_update.jl")
-include("alternating_update.jl")
-include("tdvp.jl")
-include("dmrg.jl")
-include("dmrg_x.jl")
-include("projmpo_apply.jl")
-include("contract_mpo_mps.jl")
-include("projmps2.jl")
-include("projmpo_mps2.jl")
-include("linsolve.jl")
+using Reexport: @reexport
+@reexport using ITensors.ITensorMPS: tdvp, dmrg_x, to_vec, TimeDependentSum, linsolve
+
+using ITensors: ITensorMPS
+dmrg(args...; kwargs...) = ITensorMPS.alternate_dmrg(args...; kwargs...)
+
 end
