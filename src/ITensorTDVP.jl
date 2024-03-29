7ddf3474d0453d2b00dcd24f43d6cbaa64a6539d
Miles <miles.stoudenmire@gmail.com>
2024-03-29 14:15:43 +0900

    Update src/ITensorTDVP.jl
    Co-authored-by: Matt Fishman <mtfishman@users.noreply.github.com>

--- src/ITensorTDVP.jl
+++ src/ITensorTDVP.jl
@@ -3,7 +3,7 @@ module ITensorTDVP
 using Reexport: @reexport
 @reexport using ITensors.ITensorMPS: tdvp, dmrg_x, to_vec, TimeDependentSum, linsolve
 
-using ITensors: ITensorMPS
+using ITensors.ITensorMPS: ITensorMPS
 dmrg(args...; kwargs...) = ITensorMPS.alternate_dmrg(args...; kwargs...)
 
 end
