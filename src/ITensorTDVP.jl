91cd565cbcda340be27a634982c1879d161bb883
Miles <miles.stoudenmire@gmail.com>
2024-03-31 11:51:35 -0400

    Reflect rename to ITensorMPS.alternating_update_dmrg

--- src/ITensorTDVP.jl
+++ src/ITensorTDVP.jl
@@ -4,6 +4,6 @@ using Reexport: @reexport
 @reexport using ITensors.ITensorMPS: tdvp, dmrg_x, to_vec, TimeDependentSum, linsolve
 
 using ITensors.ITensorMPS: ITensorMPS
-dmrg(args...; kwargs...) = ITensorMPS.alternate_dmrg(args...; kwargs...)
+dmrg(args...; kwargs...) = ITensorMPS.alternating_update_dmrg(args...; kwargs...)
 
 end
