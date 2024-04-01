34e4b19b9858e36dc145bdd9fc34685031073a08
Miles <miles.stoudenmire@gmail.com>
2024-04-01 13:57:38 -0400

    Update naming conventions

--- src/ITensorTDVP.jl
+++ src/ITensorTDVP.jl
@@ -1,30 +1,30 @@
 module ITensorTDVP
 
-using ITensors: ITensors, MPO, MPS
+using ITensors: ITensors
+using ITensors.ITensorMPS: ITensorMPS, MPO, MPS
 using ITensors.NDTensors: @Algorithm_str
-using ITensors.ITensorMPS: ITensorMPS
 using KrylovKit: KrylovKit
 
-function tdvp(H_generic, t::Number, psi0::MPS; kwargs...)
-  return ITensorMPS.itensortdvp_tdvp(H_generic, t, psi0; kwargs...)
+function tdvp(operator, t::Number, initial_state::MPS; kwargs...)
+  return ITensorMPS.itensortdvp_tdvp(operator, t, initial_state; kwargs...)
 end
-function tdvp(t::Number, H_generic, psi0::MPS; kwargs...)
-  return ITensorMPS.itensortdvp_tdvp(t, H_generic, psi0; kwargs...)
+function tdvp(t::Number, operator, initial_state::MPS; kwargs...)
+  return ITensorMPS.itensortdvp_tdvp(t, operator, initial_state; kwargs...)
 end
-function tdvp(H_generic, psi0::MPS, t::Number; kwargs...)
-  return ITensorMPS.itensortdvp_tdvp(H_generic, psi0, t; kwargs...)
+function tdvp(operator, initial_state::MPS, t::Number; kwargs...)
+  return ITensorMPS.itensortdvp_tdvp(operator, initial_state, t; kwargs...)
 end
-function tdvp(solver, H::MPO, t::Number, psi0::MPS; kwargs...)
-  return ITensorMPS.itensortdvp_tdvp(solver, H, t, psi0; kwargs...)
+function tdvp(solver, operator::MPO, t::Number, initial_state::MPS; kwargs...)
+  return ITensorMPS.itensortdvp_tdvp(solver, operator, t, initial_state; kwargs...)
 end
-function tdvp(solver, t::Number, H::MPO, psi0::MPS; kwargs...)
-  return ITensorMPS.itensortdvp_tdvp(solver, t, H, psi0; kwargs...)
+function tdvp(solver, t::Number, operator::MPO, initial_state::MPS; kwargs...)
+  return ITensorMPS.itensortdvp_tdvp(solver, t, operator, initial_state; kwargs...)
 end
-function tdvp(solver, psi0::MPS, t::Number, H::MPO; kwargs...)
-  return ITensorMPS.itensortdvp_tdvp(solver, psi0, t, H; kwargs...)
+function tdvp(solver, initial_state::MPS, t::Number, operator::MPO; kwargs...)
+  return ITensorMPS.itensortdvp_tdvp(solver, initial_state, t, operator; kwargs...)
 end
-function tdvp(solver, Hs::Vector{MPO}, t::Number, psi0::MPS; kwargs...)
-  return ITensorMPS.itensortdvp_tdvp(solver, Hs, t, psi0; kwargs...)
+function tdvp(solver, operators::Vector{MPO}, t::Number, initial_state::MPS; kwargs...)
+  return ITensorMPS.itensortdvp_tdvp(solver, operators, t, initial_state; kwargs...)
 end
 
 function KrylovKit.linsolve(
@@ -33,9 +33,13 @@ function KrylovKit.linsolve(
   return ITensorMPS.itensortdvp_linsolve(A, b, x0, a0, a1; kwargs...)
 end
 
-dmrg(H, psi0::MPS; kwargs...) = ITensorMPS.itensortdvp_dmrg(H, psi0; kwargs...)
+function dmrg(operator, initial_state::MPS; kwargs...)
+  return ITensorMPS.itensortdvp_dmrg(operator, initial_state; kwargs...)
+end
 
-dmrg_x(H, psi0::MPS; kwargs...) = ITensorMPS.itensortdvp_dmrg_x(H, psi0; kwargs...)
+function dmrg_x(operator, initial_state::MPS; kwargs...)
+  return ITensorMPS.itensortdvp_dmrg_x(operator, initial_state; kwargs...)
+end
 
 function ITensors.contract(alg::Algorithm"fit", A::MPO, psi::MPS; kwargs...)
   return ITensorMPS.itensortdvp_contract(alg, A, psi; kwargs...)
