48c3257f3119b4d2fafad507033b3480e824f139
Matt Fishman <mtfishman@users.noreply.github.com>
2024-05-13 15:35:46 -0400

    Update test/test_dmrg_x.jl

--- test/test_dmrg_x.jl
+++ test/test_dmrg_x.jl
@@ -32,7 +32,7 @@ using Test: @test, @test_throws, @testset
   initstate = rand(["↑", "↓"], n)
   ψ = MPS(elt, s, initstate)
   @test_throws ErrorException dmrg_x(H, ψ; nsite=2, maxdim=20, cutoff=1e-10)
-  dmrg_x_kwargs = (nsweeps=20, normalize=true, maxdim=20, cutoff=1e-10, outputlevel=0)
+  dmrg_x_kwargs = (; nsweeps=20, normalize=true, maxdim=20, cutoff=1e-10, outputlevel=0)
   e, ϕ = dmrg_x(H, ψ; nsite=2, dmrg_x_kwargs...)
   @test ITensors.scalartype(ϕ) == elt
   @test inner(ϕ', H, ϕ) / inner(ϕ, ϕ) ≈ e
