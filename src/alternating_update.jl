7085bb96dbd44c5fe97ccd936335af5be58a2361
Matt Fishman <mtfishman@users.noreply.github.com>
2024-09-04 18:45:29 -0400

    Pass observers to `checkdone(...)`

--- src/alternating_update.jl
+++ src/alternating_update.jl
@@ -105,7 +105,7 @@ function alternating_update(
       println()
       flush(stdout)
     end
-    isdone = checkdone(; state, sweep, outputlevel)
+    isdone = checkdone(; state, sweep, outputlevel, observer=observer!, sweep_observer=sweep_observer!)
     isdone && break
   end
   return state
